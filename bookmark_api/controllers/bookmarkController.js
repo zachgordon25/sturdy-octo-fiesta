46c39efad847da4cc6a50f7964d0ec1357d93570
Ben Burke <benjaminstevenburke@gmail.com>
2019-08-15 16:57:18 -0400

    starting controller

--- bookmark_api/controllers/bookmarkController.js
+++ bookmark_api/controllers/bookmarkController.js
@@ -3,7 +3,7 @@ const bookmarkController = express.Router()
 const bookmarkModel = require('../models/bookmarkModel.js')
 
 bookmarkController.get('/', (req, res) =>{
-    bookmarkModel.find{}
+   
 })
 
 module.exports = bookmarkController;
