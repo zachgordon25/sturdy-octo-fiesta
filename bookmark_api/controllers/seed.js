baa92e2030934fe8a2e43ae11f6cd41f600be573
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-08-15 17:13:06 -0400

    seed data

---
+++ bookmark_api/controllers/seed.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    title: 'Google',
+    url: 'https://www.google.com/'
+  },
+  {
+    title: 'w3schools',
+    url: 'https://www.w3schools.com/'
+  },
+  {
+    title: 'MDN',
+    url: 'https://developer.mozilla.org/en-US/'
+  },
+  {
+    title: 'Stack Overflow',
+    url: 'https://stackoverflow.com/'
+  },
+  {
+    title: 'Reddit',
+    url: 'https://reddit.com/'
+  },
+  {
+    title: 'CSS Tricks',
+    url: 'https://css-tricks.com/'
+  },
+  {
+    title: 'Bootstrap',
+    url: 'https://getbootstrap.com/'
+  },
+  {
+    title: 'Skeleton',
+    url: 'http://getskeleton.com/'
+  }
+];
