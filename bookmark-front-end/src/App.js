67172a3fd8986e5657eeafe974e2f9ac345ead71
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-08-15 18:55:57 -0400

    added to app.js

--- bookmark-front-end/src/App.js
+++ bookmark-front-end/src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import NewForm from './components/NewFrom.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
