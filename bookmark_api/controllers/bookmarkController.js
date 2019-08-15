a9d802fb1d616abda098cee121abf3b212062bd2
Ben Burke <benjaminstevenburke@gmail.com>
2019-08-15 17:00:58 -0400

    added res send

--- bookmark_api/controllers/bookmarkController.js
+++ bookmark_api/controllers/bookmarkController.js
@@ -3,7 +3,7 @@ const bookmarkController = express.Router()
 const bookmarkModel = require('../models/bookmarkModel.js')
 
 bookmarkController.get('/', (req, res) =>{
-   
+   res.send('hello')
 })
 
 module.exports = bookmarkController;
