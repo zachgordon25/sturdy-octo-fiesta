bf512ba9b33f2bec90a455c020fbba0b27d300b6
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-15 19:10:13 -0400

    app.js boilerplate

--- bookmark-front-end/src/App.js
+++ bookmark-front-end/src/App.js
@@ -4,25 +4,16 @@ import logo from './logo.svg';
 import './App.css';
 import NewForm from './components/NewForm.js';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+  render() {
+    return (
+      <div className="container"></div>
+    )
+  }
 }
 
 export default App;
