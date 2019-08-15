102660add1106c44bd27d168a9668a01f8896bce
Ben Burke <benjaminstevenburke@gmail.com>
2019-08-15 17:32:41 -0400

    seed router added

--- bookmark_api/controllers/bookmarkController.js
+++ bookmark_api/controllers/bookmarkController.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const bookmarkController = express.Router()
 const bookmarkModel = require('../models/bookmarkModel.js')
+const bookmarkSeed = require ( '../models/seed.js');
 
-bookmarkController.get('/', (req, res) => {
-   res.send('hello')
+bookmarkController.get('/', (req, res) =>{
+   bookmarkModel.find({}, (err, foundBookmark) =>{
+       if (err) {
+           res.status
+       }
+   })
 })
+
+bookmarkController.get ( '/seed' , ( req, res ) => {
+  bookmarkController.insertMany ( bookmarkSeed, ( err , bookmark ) =>{
+    if (err) { console.log ( err ); } else {
+     res.send (bookmark );
+    }
+  });
+});
+
+
 
 module.exports = bookmarkController;
