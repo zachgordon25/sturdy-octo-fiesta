16dcb0303bc03e726361bc683dc16fbf402788a4
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-08-15 18:58:17 -0400

    fixed typo

--- bookmark-front-end/src/App.js
+++ bookmark-front-end/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
-import NewForm from './components/NewFrom.js';
+import NewForm from './components/NewForm.js';
 
 function App() {
   return (
