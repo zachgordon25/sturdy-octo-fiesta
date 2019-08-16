60e90054e6cb0f3654c0eeec5312f8dae31fd798
Ben Burke <benjaminstevenburke@gmail.com>
2019-08-16 15:09:37 -0400

    baseURL added to new Form

--- bookmark-front-end/src/components/newForm.js
+++ bookmark-front-end/src/components/newForm.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+let baseURL = process.env.REACT_APP_BASEURL;
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003';
+} else {
+  baseURL = 'https://fathomless-sierra-68956.herokuapp.com';
+}
+
 class NewForm extends Component {
     constructor(props) {
         super(props)
