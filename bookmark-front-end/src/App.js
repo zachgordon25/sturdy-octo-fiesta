63559811ea626c2a805ad434147654d3cc1d2a1d
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-16 14:44:18 -0400

    attempting map function

--- bookmark-front-end/src/App.js
+++ bookmark-front-end/src/App.js
@@ -3,42 +3,51 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 // import NewForm from './components/NewForm.js';
-let baseURL = process.env.REACT_APP_BASEURL
+let baseURL = process.env.REACT_APP_BASEURL;
 
 if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:3003'
+  baseURL = 'http://localhost:3003';
 } else {
-  baseURL = 'https://fathomless-sierra-68956.herokuapp.com'
+  baseURL = 'https://fathomless-sierra-68956.herokuapp.com';
 }
 
 console.log('current base URL:', baseURL);
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       title: ''
-    }
-    this.getBookmarks = this.getBookmarks.bind(this)
+    };
+    this.getBookmarks = this.getBookmarks.bind(this);
   }
 
   async getBookmarks() {
-    const response = await axios(`${baseURL}/bookmark`)
-    const data = response.data
+    const response = await axios(`${baseURL}/bookmark`);
+    const title = response.data;
     this.setState({
-      title: data
-    })
+      title: title
+    });
     console.log(this.state.title);
   }
   componentDidMount() {
-    this.getBookmarks()
+    this.getBookmarks();
   }
 
   render() {
     return (
-      <div className="container">
+      <div className='container'>
         <h1>Test</h1>
+        <table>
+          {this.state.bookmarks.map(bookmarks => {
+            return (
+              <tr>
+                <td>{bookmarks.title}</td>
+              </tr>
+            );
+          })}
+        </table>
       </div>
-    )
+    );
   }
 }
 
