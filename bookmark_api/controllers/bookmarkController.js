d37f21ed74ca95a1330d2871a5c337698898e120
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-08-19 08:14:38 -0400

    come and get it

--- bookmark_api/controllers/bookmarkController.js
+++ bookmark_api/controllers/bookmarkController.js
@@ -4,37 +4,37 @@ const bookmarkModel = require('../models/bookmarkModel.js')
 const bookmarkSeed = require('../models/seed.js');
 
 bookmarkController.get('/', (req, res) => {
-   bookmarkModel.find({}, (err, foundBookmark) => {
-      if (err) {
-         res.status(400).json({ error: err.message })
-      } else {
-         res.status(200).json(foundBookmark)
-      }
-   })
+    bookmarkModel.find({}, (err, foundBookmark) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+        } else {
+            res.status(200).json(foundBookmark)
+        }
+    })
 })
 
-bookmarkController.post('/', (req, res)=>{
-    bookmarkModel.create(req.body, (error, createdBookmark) =>{
+bookmarkController.post('/', (req, res) => {
+    bookmarkModel.create(req.body, (error, createdBookmark) => {
         if (error) {
-            res.status(400).json({ error: error.message})
+            res.status(400).json({ error: error.message })
         }
         res.status(200).send(createdBookmark)
     })
 })
 
-bookmarkController.delete('/:id', (req, res)=>{
-    bookmarkModel.findByIdAndRemove(req.params.id,(err, deletedBookmark)=>{
+bookmarkController.delete('/:id', (req, res) => {
+    bookmarkModel.findByIdAndRemove(req.params.id, (err, deletedBookmark) => {
         if (err) {
-            res.status(400).json({ error: err.message})
+            res.status(400).json({ error: err.message })
         }
         res.status(200).json(deletedBookmark)
     })
 })
 
-bookmarkController.put('/:id', (req, res) =>{
-    bookmarkController.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedBookmark) =>{
+bookmarkController.put('/:id', (req, res) => {
+    bookmarkController.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedBookmark) => {
         if (err) {
-            res.status(400).json({ error: err.message})
+            res.status(400).json({ error: err.message })
         }
         res.status(200).json(updatedBookmark)
     })
@@ -47,7 +47,6 @@ bookmarkController.put('/:id', (req, res) =>{
 //       }
 //    });
 // });
-
 
 
 module.exports = bookmarkController;
